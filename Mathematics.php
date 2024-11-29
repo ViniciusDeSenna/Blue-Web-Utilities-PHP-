adc2961efe4a6694bd283257e2df81c5cb4d5208
Vinícius De Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2024-11-29 18:09:04 -0300

    Calculando o MMC

--- Mathematics.php
+++ Mathematics.php
@@ -1,10 +1,16 @@
 <?php
 class Mathematics
-{
-    public static function test(){
-        return 'teste';
+{   
+    public static function Multiply(array $numbers) {
+        $result = 1;  
+
+        foreach ($numbers as $number) {
+            $result *= $number;
+        }
+
+        return $result;
     }
-    
+
     public static function directlyProportional(array $prop = [0], float $value = 0): array
     {
         $propValue = array_sum($prop);
@@ -32,31 +38,37 @@ class Mathematics
      */
     public static function MDC(int $i1, int $i2): int
     {
-        $a = ($i1 >= $i2) ? $i1 : $i2; #largest number 
-        $b = ($i2 >= $i1) ? $i2 : $i1; #smallest number
+        $a = ($i1 >= $i2) ? $i1 : $i2; #largest number
+        $b = ($i2 <= $i1) ? $i2 : $i1; #smallest number
 
-        $r = null;
-        while($r != 0){
-            $r = $a % $b;
-            $a = $r;
+        $r = 1;
+        while ($r != 0) {
+            $r = $a % $b; 
+            $a = $b;
+            $b = $r;
         }
 
         return $a;
     }
 
-    public static function MMC(array $numbers): int
+    public static function MMC(float $n1, float $n2): float
     {
-        $i = count($numbers);
+        $mdc = self::MDC($n1, $n2);
+        return self::Multiply([$n1,$n2]) / $mdc;
+    }
 
-        while($i != 1){
-            $i = count($numbers);
-            $mdc = self::MDC($numbers[0], $numbers[1]);
+    public static function MultMMC(array $numbers) {
+
+        while(count($numbers) > 1){
+
+            $mmc = self::MMC($numbers[0], $numbers[1]);
             unset($numbers[0]);
             unset($numbers[1]);
-            array_unshift($numbers, $mdc);
+            array_unshift($numbers, $mmc);
+
         };
 
-        return $numbers[0];
+        return $mmc;
     }
 
     // public function POW($number, $pow){
@@ -64,3 +76,11 @@ class Mathematics
     // }
 }
 ?>
+
+<?php
+
+echo "Testando o MDC" . "\n";
+echo "MDC de 56 e 98: " . Mathematics::MDC(56, 98) . "\n";
+
+echo "Testando o MMC" . "\n";
+echo "MMC de [4, 5, 6]: " . Mathematics::MultMMC([4, 5, 6]) . "\n";
